1337807c899c9eb54c0d247d3e98a2d41faf3b7b
david Huang AMP <david_huang@amp.com.au>
2022-09-01 18:38:46 +1000

    add new proj

---
+++ longjob/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.extensions.DependencyInjection;
+
+
+
+
+// Task.Factory.StartNew(async ()=>{
+//     while(true){
+// Console.WriteLine("long run!!");
+// await Task.Delay(1000);
+//     }
+// });
+
+
+// //Console.WriteLine("Hello, World!");
+
+
+// Console.ReadLine();
